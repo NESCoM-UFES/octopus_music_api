fd3d589b65991f2bd514793789f6f73f4a89fa9f
Leandro Costalonga <llcostalonga@gmail.com>
2012-04-03 21:44:56 -0300

    Primeiro Commit

---
+++ TesteEGIT/src/AppTesteEGIT.java
@@ -0,0 +1,17 @@
+
+public class AppTesteEGIT {
+	//add your method too!
+		public static void llcostalongaSaysDoWhatever(){
+			System.out.println("llcostalonga says: whatever!");
+		}
+		
+		public static void main(String[] args) {
+		  /*How this will work: Every developer just write a line similar to the one bellow. 
+		   * Then, add your own method to do whatever. The goal is to see how GIT merge things.
+		   */
+			
+			System.out.println("llcostalonga wrote this!");
+			llcostalongaSaysDoWhatever();
+
+		}
+}
