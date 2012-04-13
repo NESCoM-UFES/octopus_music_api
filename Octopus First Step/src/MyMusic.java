1099841bf87c0b0c06b2ae663c8022f58114437f
Leandro Costalonga <llcostalonga@gmail.com>
2012-04-13 17:02:34 -0300

    Let's start play!

---
+++ Octopus First Step/src/MyMusic.java
@@ -0,0 +1,27 @@
+import examples.PlayableThings;
+import octopus.*;
+
+public class MyMusic {
+
+	public static void main(String[] args) {
+		try{
+			Musician musician = new Musician();
+			Harmony harmony;
+
+			harmony = PlayableThings.getHarmonyToPlay();
+			System.out.println(harmony); 
+			musician.play(harmony);
+		} catch (ChordNotationException e) {
+			System.out.println("Ops...it seems that your musician is having troubles with the notes of the harmony: ");
+		}
+
+		catch (MusicPerformanceException ex) {
+			System.out.println("Ops...it seems that your musician had troubles: " +  ex.getMessage());
+		}catch (Exception e) {
+			System.out.println("Ops...it seems that your musician had troubles: " +  e.getMessage());
+		}finally{
+			//Runtime.getRuntime().exit(0);
+		}
+	}
+
+}
